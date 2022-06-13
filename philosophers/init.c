6a39003118d9a1f2f821e7dcd379103bc8520671
Raphlecrabe <98089221+Raphlecrabe@users.noreply.github.com>
2022-06-13 08:33:17 +0200

    Add files via upload

---
+++ philosophers/init.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmonacho <rmonacho@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 15:30:00 by rmonacho          #+#    #+#             */
+/*   Updated: 2022/06/09 16:16:10 by rmonacho         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incs/philo.h"
+
+unsigned long long	ft_gettime(void)
+{
+	struct timeval		time;
+	unsigned long long	ms;
+
+	gettimeofday(&time, NULL);
+	ms = (time.tv_sec * 1000000) + (time.tv_usec);
+	return (ms);
+}
+
+void	ft_init2(t_data *datas, char **argv, int argc)
+{
+	datas->state = 1;
+	datas->amount = ft_atoi(argv[1]);
+	datas->time_die = ft_atoi(argv[2]) * 1000;
+	datas->time_eat = ft_atoi(argv[3]) * 1000;
+	datas->time_sleep = ft_atoi(argv[4]) * 1000;
+	if (argc == 6)
+		datas->maxmeals = ft_atoi(argv[5]);
+	else
+		datas->maxmeals = -1;
+	datas->current = 0;
+}
+
+int	ft_init(t_data **datas, char **argv, int argc)
+{
+	int	i;
+
+	i = 0;
+	*datas = malloc(sizeof(t_data));
+	if (*datas == NULL)
+		return (-1);
+	ft_init2(*datas, argv, argc);
+	(*datas)->philo = malloc(sizeof(t_philo) * ((*datas)->amount));
+	if ((*datas)->philo == NULL)
+		return (-1);
+	(*datas)->start = ft_gettime();
+	pthread_mutex_init(&(*datas)->msgs, NULL);
+	pthread_mutex_init(&(*datas)->protectstate, NULL);
+	(*datas)->state = 1;
+	(*datas)->message = 1;
+	while (i < (*datas)->amount)
+	{
+		(*datas)->philo[i].nbr = i + 1;
+		(*datas)->philo[i].lastmeal = (*datas)->start;
+		(*datas)->philo[i].nbrfeast = 0;
+		(*datas)->philo[i].fork = 0;
+		pthread_mutex_init(&(*datas)->philo[i].lockfork, NULL);
+		pthread_mutex_init(&(*datas)->philo[i].lockmeal, NULL);
+		pthread_mutex_init(&(*datas)->philo[i].locklast, NULL);
+		pthread_mutex_init(&(*datas)->philo[i].lockdead, NULL);
+		(*datas)->philo[i].datas = *datas;
+		(*datas)->philo[i].isdead = 0;
+		i++;
+	}
+	return (0);
+}
---
+++ philosophers/main.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmonacho <rmonacho@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/25 13:06:30 by rmonacho          #+#    #+#             */
+/*   Updated: 2022/06/09 17:08:19 by rmonacho         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incs/philo.h"
+
+int	ft_test(t_data *datas)
+{
+	unsigned long long	time;
+	int					i;
+	int					eat;
+
+	i = 0;
+	eat = 1;
+	while (i < datas->amount)
+	{
+		pthread_mutex_lock(&datas->philo[i].lockmeal);
+		if (datas->maxmeals != -1 && datas->philo[i].nbrfeast < datas->maxmeals)
+			eat = 0;
+		pthread_mutex_unlock(&datas->philo[i].lockmeal);
+		pthread_mutex_lock(&datas->philo[i].locklast);
+		time = ft_gettime();
+		if (time - datas->philo[i].lastmeal
+			>= (unsigned long long) datas->time_die)
+		{
+			pthread_mutex_unlock(&datas->philo[i].locklast);
+			time = ft_gettime();
+			ft_message("died", &datas->philo[i],
+				ft_realtime(&datas->philo[i], time));
+			pthread_mutex_lock(&datas->protectstate);
+			datas->state = 0;
+			pthread_mutex_unlock(&datas->protectstate);
+			pthread_mutex_lock(&datas->philo[i].lockdead);
+			datas->philo[i].isdead = 1;
+			pthread_mutex_unlock(&datas->philo[i].lockdead);
+			return (0);
+		}
+		pthread_mutex_unlock(&datas->philo[i].locklast);
+		i++;
+		usleep(500);
+	}
+	pthread_mutex_lock(&datas->msgs);
+	if (datas->maxmeals != -1 && eat == 1)
+	{
+		datas->state = 0;
+		return (0);
+	}
+	pthread_mutex_unlock(&datas->msgs);
+	return (1);
+}
+
+void	*ft_routine(void *arg)
+{
+	t_philo	*philo;
+
+	philo = arg;
+	if (philo->nbr % 2 == 0)
+		ft_usleep(philo->datas->time_eat);
+	while (1)
+	{
+		if (ft_isok(philo) == -1)
+			return (0);
+		if (ft_eat(philo) == -1)
+			return (0);
+		if (ft_thinksleep(philo) == -1)
+			return (0);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	t_data	*datas;
+
+	i = 0;
+	datas = NULL;
+	if (argc < 5 || argc > 6)
+	{
+		printf("%s\n", "error : bad arguments, pls be smart");
+		return (0);
+	}
+	if (ft_init(&datas, argv, argc) == -1)
+	{
+		//freeall
+		return (-1);
+	}
+	while (i < datas->amount)
+	{
+		pthread_create(&datas->philo[i].thread, NULL,
+			&ft_routine, &datas->philo[i]);
+		i++;
+	}
+	while (1)
+	{
+		if (ft_test(datas) == 0)
+			break ;
+	}
+	i = 0;
+	while (i < datas->amount)
+	{
+		pthread_join(datas->philo[i].thread, NULL);
+		i++;
+	}
+	return (0);
+}
